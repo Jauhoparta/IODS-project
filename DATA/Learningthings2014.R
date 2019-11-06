0af120b9feac09f49b498314d52601533a391077
Tuomas Mäkelä <tuomas.makela@munkka.fi>
2019-11-06 16:44:17 +0200

    Now some actual data

--- DATA/Learningthings2014.R
+++ DATA/Learningthings2014.R
@@ -1,1 +1,5 @@
 #Tuomas Mäkelä 6.11. Learning data set of much WOW.
+
+Opiskelu<-read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", header=T, sep="\t")
+#Data on learning with 60 variables and 183 observations in each variable. 
+#Data points integers 1 to 5 
