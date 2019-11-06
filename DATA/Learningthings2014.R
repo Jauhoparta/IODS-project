b63680cfed8e45d6903dbe7afed7a45cf3f08c18
Tuomas Mäkelä <tuomas.makela@munkka.fi>
2019-11-06 18:14:49 +0200

    Still going

--- DATA/Learningthings2014.R
+++ DATA/Learningthings2014.R
@@ -30,4 +30,7 @@ Opiskelu<-select(Opiskelu, one_of(keep_columns))
 #Scaling Attitude to nice things
 Opiskelu$Attitude<-Opiskelu$Attitude/10
 #Writing the data into the required location
-write.table(Opiskelu, file="LRN14.csv")
+write.csv(Opiskelu, file="LRN14.csv")
+
+#let's see if we can read teh data now:
+UUSI<-read.table("~//LRN14.csv", header=T)
