f5884ee0ffce31b58e2ca0a73c041b80c6e11dfd
Tuomas Mäkelä <tuomas.makela@munkka.fi>
2019-11-24 22:56:41 +0200

    Data wrangling for next week. Renames all done.

---
+++ create_human.R
@@ -0,0 +1,43 @@
+# Doing some data wrangling for next week now:
+
+library(dplyr)
+# Reading data into memory
+
+
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+
+#Now lets look at the data
+
+str(hd)
+str(gii)
+
+#and summarize the variables:
+
+summary(hd)
+summary(gii)
+
+# Renaming some of the difficult names of variables now (varible names Capitalized, to avoid error) and look at teh results.
+# First table hd
+hd<-rename(hd, HDI=Human.Development.Index..HDI.)
+summary(hd)
+
+
+hd<-rename(hd, GNIperc=Gross.National.Income..GNI..per.Capita)
+hd<-rename(hd, Edumean=Mean.Years.of.Education)
+hd<-rename(hd, Eduexp=Expected.Years.of.Education  )
+hd<-rename(hd, Lifeexp=Life.Expectancy.at.Birth  )
+hd<-rename(hd, GminusHDI=GNI.per.Capita.Rank.Minus.HDI.Rank)
+summary(hd)
+
+#Then table gii. again, taking names that are short and descripive
+str(gii)
+gii<-rename(gii, Gineq=Gender.Inequality.Index..GII.)
+gii<-rename(gii, MMRat=Maternal.Mortality.Ratio )
+gii<-rename(gii,Adbirth=Adolescent.Birth.Rate )
+gii<-rename(gii,Repparl=Percent.Representation.in.Parliament )
+gii<-rename(gii,Edfemale=Population.with.Secondary.Education..Female.)
+gii<-rename(gii,Edmale=Population.with.Secondary.Education..Male.)
+gii<-rename(gii,Labfem=Labour.Force.Participation.Rate..Female. )
+gii<-rename(gii,Labmale=Labour.Force.Participation.Rate..Male. )
+
