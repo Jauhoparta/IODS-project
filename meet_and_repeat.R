81e885c4759629a0820015a9a08e63534890757c
Tuomas Mäkelä <tuomas.makela@munkka.fi>
2019-12-08 22:16:33 +0200

    Data wrangling for the week.

---
+++ meet_and_repeat.R
@@ -0,0 +1,31 @@
+#Read the data into memory now.
+
+BPRS<-read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", sep=" ", header=T)
+
+rats<-read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", header=T, sep="\t")
+
+#Converting categorical variables into factors:
+BPRS$treatment<-as.factor(BPRS$treatment)
+
+BPRS$subject <- as.factor(BPRS$subject)
+rats$ID<-as.factor(rats$ID)
+rats$Group<-as.factor(rats$Group)
+
+#Wrangling. initialize the required libraries
+library(dplyr)
+library(tidyr)
+
+
+
+# Convert to long form both the datasets
+BPRSL <-  BPRS %>% gather(key = weeks, value = bprs, -treatment, -subject)
+BPRSL <-  BPRSL %>% mutate(week = as.integer(substr(weeks,5,5)))
+
+
+RATSL <- rats %>%
+  gather(key = WD, value = Weight, -ID, -Group) %>%
+  mutate(Time = as.integer(substr(WD,3,4))) 
+
+# Now the data are in long form. this means that both for BPRSL (the long one) and RATSL, we have fewer variables than before.
+# The variables are, for RATSL: ID, Groupl, Woéight and time while for BPRSL they are treatment, subject, bprsl and week. 
+# The variable weeks is redundant since we already have the same info in week.
